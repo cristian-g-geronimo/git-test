32a4ac6f876bc12085ebb24ebc75bd747b8e6d51
CristianGheorghe <cristian.gheorghe@geronimostudios.com>
2017-12-08 09:06:48 +0200

    inital commit

---
+++ app/src/main/java/com/geronimo/gittest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.geronimo.gittest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by cgheorg1 on 08.12.2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
